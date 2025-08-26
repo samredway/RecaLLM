e21ea0f450587241a459151747ad180a38a286f6
Sam Redway <sam.redway@outlook.com>
2025-08-26 20:02:47 +0200

    make shortTermMemory type explicit

--- src/main/java/com/redway/recallm/services/ChatOrchestratorService.java
+++ src/main/java/com/redway/recallm/services/ChatOrchestratorService.java
@@ -2,6 +2,7 @@ package com.redway.recallm.services;
 
 import com.redway.recallm.api.dtos.ChatRequest;
 import com.redway.recallm.models.Memory;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,7 +20,7 @@ public class ChatOrchestratorService {
     var newUserMemory =
         new Memory(request.userId(), request.sessionId(), Memory.Role.USER, request.message());
     memoryService.memorise(newUserMemory);
-    var shortTermMemory = memoryService.recallSession(request.sessionId());
+    List<Memory> shortTermMemory = memoryService.recallSession(request.sessionId());
     String prompt = promptService.generatePromptFromShortTermMemory(shortTermMemory);
     String answer = chatService.chat(prompt);
     var newAssistantMemory =
