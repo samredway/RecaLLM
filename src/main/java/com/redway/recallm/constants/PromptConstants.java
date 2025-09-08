d6b6a15e545b3af3129c5c44566bd122b9805f4d
Sam Redway <sam.redway@outlook.com>
2025-09-08 20:28:32 +0100

    improve prompts

--- src/main/java/com/redway/recallm/constants/PromptConstants.java
+++ src/main/java/com/redway/recallm/constants/PromptConstants.java
@@ -10,12 +10,14 @@ public final class PromptConstants {
    */
   public static final String SUMMARY_PROMPT =
       "Please take the following text and summarise it in such a way that you capture all the key"
-          + " points and can continue a conversation without losing context but also reducing it to"
-          + " a more concises form.\n"
-          + " This summary should be no more than 500 words, so if the full text is less than 500"
-          + " words then keep the whole text. Otherwise you will have to squash it down keeping the"
-          + " main points. You should only return the summary itself and no other text. This"
-          + " summary is used by a programatic process\n\n";
+          + " points or what we discussed. This summary will be used as part of the prompt for our"
+          + " very next conversation. You should present all key points clearly - perhaps using"
+          + " headings and bullet points, and add as much detail as required so as to not lose"
+          + " important context. This summary should be no more than 500 words You should only"
+          + " return the summary itself and no other text. This summary is used by a programatic"
+          + " process\n\n";
 
-  public static final String SESSION_SUMMARY_PREFIX = "Here is a summary of our last session:\n\n";
+  public static final String SESSION_SUMMARY_PREFIX =
+      "Here is a summary of our last session. This is your memory of the last conversation you had"
+          + " with me:\n\n";
 }
